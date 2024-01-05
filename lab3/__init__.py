181d43b8e249cdc3df39a7a71310b1561f878562
raiten16 <64745859+raiten16@users.noreply.github.com>
2024-01-05 16:46:08 +0200

    Update __init__.py

--- lab3/__init__.py
+++ lab3/__init__.py
@@ -5,7 +5,7 @@ from flask_migrate import Migrate
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgres://lab3_yc5s_user:MTWf88j3aP3rToTHoCyujLF8ENg5NGtP@dpg-cmc15i6n7f5s7394tmag-a.oregon-postgres.render.com/lab3_yc5s"
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://lab3_yc5s_user:MTWf88j3aP3rToTHoCyujLF8ENg5NGtP@dpg-cmc15i6n7f5s7394tmag-a.oregon-postgres.render.com/lab3_yc5s"
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
